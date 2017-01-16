c253bc3f4bd818de57eb9c1c8965b1cbb729bb35
Sang Song <songaal@gmail.com>
2017-01-16 21:04:34 +0900

    클라이언트 서버 양방향 통신성공.

--- src/main/java/io/gncloud/tcp/VertxTcpServerVerticle.java
+++ src/main/java/io/gncloud/tcp/VertxTcpServerVerticle.java
@@ -4,7 +4,8 @@ import io.vertx.core.*;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.net.NetServer;
 import io.vertx.core.net.NetSocket;
-
+import java.util.Date;
+import java.util.Random;
 
 /**
  * Created by swsong on 2017. 1. 13..
@@ -17,26 +18,36 @@ public class VertxTcpServerVerticle extends AbstractVerticle {
         vertx.deployVerticle(new VertxTcpServerVerticle(),new DeploymentOptions().setWorker(true));
     }
 
+
+
+
     @Override
     public void start() throws Exception {
         NetServer server = vertx.createNetServer();
 
         server.connectHandler(new Handler<NetSocket>() {
 
+            RandomMessageMan mm;
+
             @Override
             public void handle(NetSocket netSocket) {
                 System.out.println("Incoming connection!");
 
+                String welcomeMessage = "어서옵쇼. 서버에 접속성공! " + (new Date());
+                netSocket.write(welcomeMessage);
+
+                mm = new RandomMessageMan(netSocket);
+                mm.start();
                 netSocket.handler(new Handler<Buffer>() {
 
                     @Override
                     public void handle(Buffer buffer) {
                         System.out.println("incoming data len : "+buffer.length());
 
-                        String data = buffer.getString(0, 4);
-                        int num = buffer.getInt(4);
+                        int len = buffer.length();
+                        String data = buffer.getString(0, len);
 
-                        System.out.println("incoming data : "+data + " , " + num);
+                        System.out.println("incoming data : "+data);
 
                         Buffer outBuffer = Buffer.buffer();
                         outBuffer.appendString("response...");
@@ -47,17 +58,27 @@ public class VertxTcpServerVerticle extends AbstractVerticle {
             }
         });
 
-//        server.close(new Handler<AsyncResult<Void>>() {
-//            @Override
-//            public void handle(AsyncResult result) {
-//                if(result.succeeded()){
-//                    //TCP server fully closed
-//                    System.out.println("Closed.. "+result.toString());
-//                }
-//            }
-//        });
-
         server.listen(10000);
         System.out.println("Listen on 10000..");
+    }
+}
+
+class RandomMessageMan extends Thread {
+    NetSocket netSocket;
+    Random r = new Random();
+
+    public RandomMessageMan(NetSocket netSocket) {
+        this.netSocket = netSocket;
+    }
+
+    public void run() {
+        while(true) {
+            netSocket.write("The time is " + new Date());
+            try {
+                Thread.sleep((r.nextInt(9000) + 1000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/io/gncloud/tcp/client.py
+++ src/main/java/io/gncloud/tcp/client.py
@@ -1,23 +1,130 @@
-import binascii
-import sys
-import socket
-import struct
-import sys
+# Copyright (c) Twisted Matrix Laboratories.
+# See LICENSE for details.
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-values = ('abdf', 27)
-packer = struct.Struct('>4s i')
-packed_data = packer.pack(*values)
+"""
+An example IRC log bot - logs a channel's events to a file.
 
-sock.connect( ('localhost', 10000) )
+If someone says the bot's name in the channel followed by a ':',
+e.g.
 
-try:
+    <foo> logbot: hello!
 
-    # Send data
-    print >>sys.stderr, 'sending "%s"' % binascii.hexlify(packed_data), values
-    sock.sendall(packed_data)
+the bot will reply:
 
-finally:
-    print >>sys.stderr, 'closing socket'
-    sock.close()
+    <logbot> foo: I am a log bot
+
+Run this script with two arguments, the channel name the bot should
+connect to, and file to log to, e.g.:
+
+    $ python ircLogBot.py test test.log
+
+will log channel #test to the file 'test.log'.
+
+To run the script:
+
+    $ python ircLogBot.py <channel> <file>
+"""
+
+
+from __future__ import print_function
+
+# twisted import
+from twisted.internet import reactor, protocol
+from twisted.python import log
+from twisted.internet.protocol import Protocol
+
+# system imports
+import time, sys
+
+from random import randint
+from time import sleep
+from threading import Thread
+
+# sleep(randint(10,100))
+
+
+class RandomMessageMan(Thread):
+    def __init__(self, channel):
+        self.channel = channel
+        Thread.__init__(self)
+        self.daemon = True
+        self.start()
+    def run(self):
+        while True:
+            print("thread wrote...")
+            i = randint(1,10)
+            sleep(i)
+            self.channel.write("%d hi server" % i)
+
+
+class MessageLogger:
+    """
+    An independent logger class (because separation of application
+    and protocol logic is a good thing).
+    """
+    def __init__(self, file):
+        self.file = file
+
+    def log(self, message):
+        """Write a message to the file."""
+        timestamp = time.strftime("[%H:%M:%S]", time.localtime(time.time()))
+        self.file.write('%s %s\n' % (timestamp, message))
+        self.file.flush()
+
+    def close(self):
+        self.file.close()
+
+
+class AgentProtocol(Protocol):
+
+    def dataReceived(self, data):
+        print("received > ", data)
+
+
+    def connectionMade(self):
+        self.logger = MessageLogger(open(self.factory.filename, "a"))
+        self.logger.log("[connected at %s]" %
+                        time.asctime(time.localtime(time.time())))
+        self.transport.write("Connected!")
+        RandomMessageMan(self.transport)
+
+    def connectionLost(self, reason):
+        self.logger.log("[disconnected at %s]" %
+                        time.asctime(time.localtime(time.time())))
+        self.logger.close()
+
+
+class AgentFactory(protocol.ClientFactory):
+    """A factory for LogBots.
+
+    A new protocol instance will be created each time we connect to the server.
+    """
+
+    def __init__(self, channel, filename):
+        self.channel = channel
+        self.filename = filename
+        print("_INIT_")
+
+    def buildProtocol(self, addr):
+        p = AgentProtocol()
+        p.factory = self
+        return p
+
+    def clientConnectionLost(self, connector, reason):
+        connector.connect()
+
+    def clientConnectionFailed(self, connector, reason):
+        print("connection failed:", reason)
+        reactor.stop()
+
+
+if __name__ == '__main__':
+    # initialize logging
+    log.startLogging(sys.stdout)
+    # create factory protocol and application
+    f = AgentFactory("a", "a")
+    # connect factory to this host and port
+    reactor.connectTCP("localhost", 10000, f)
+    # run bot
+    reactor.run()
